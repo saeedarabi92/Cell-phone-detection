c91e445631b5652cc9b035ee35f838ad15420924
saeedarabi92 <arabi@iastate.edu>
2021-02-03 18:25:54 -0600

    Update find_phone.py

--- find_phone.py
+++ find_phone.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     check_python_version()
     img_path = sys.argv[1]
     phone_detector = Phone_Detector(
-        save_image=False, debug=False, summerize_result=False)
+        save_image=False, debug=False)
     img = cv2.imread(img_path)
     x, y = phone_detector.feed(img, img_path)
     print(round(x, 4), " ", round(y, 4))
