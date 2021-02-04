1cf0efc0a6367d5592a551ebff4e85e9155daf80
saeedarabi92 <arabi@iastate.edu>
2021-02-03 18:25:28 -0600

    update generate_detection_results.py

--- generate_detection_results.py
+++ generate_detection_results.py
@@ -17,22 +17,10 @@ if __name__ == '__main__':
     #  60, 48, 109, 41
     check_python_version()
     phone_detector = Phone_Detector(
-        save_image=True, debug=True, summerize_result=True)
+        save_image=True, debug=True)
     # test_files = glob.glob("./data/find_phone/*.jpg")
     for test_file in test_files:
         test_image = cv2.imread(test_file)
         phone_detector.feed(test_image, test_file)
 
-    if phone_detector.summerize_result:
-        print("phone_detector.correct_counts: ",
-              phone_detector.correct_counts, "\n",
-              "phone_detector.incorrect_counts: ",
-              phone_detector.incorrect_counts, "\n",
-              "phone_detector.accuracy: ", phone_detector.correct_counts * 100 / (phone_detector.correct_counts +
-                                                                                  phone_detector.incorrect_counts), "%", "\n"
-              "phone_detector.files_detected_by_deep_model: ",
-              phone_detector.files_detected_by_deep_model, "\n",
-              "phone_detector.files_detected_by_color_filtering: ",
-              phone_detector.files_detected_by_color_filtering, "\n",
-              "phone_detector.files_with_no_detection: ",
-              phone_detector.files_with_no_detection, "\n")
+    phone_detector.summerize_result()
