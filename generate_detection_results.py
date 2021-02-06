4d45cd95cf7df827e5b08ed3c81f4334ed9c54a5
saeed <arabi@iastate.edu>
2021-02-06 12:12:51 -0600

    update py file

--- generate_detection_results.py
+++ generate_detection_results.py
@@ -10,17 +10,20 @@ from helper import Phone_Detector, check_python_version
 
 
 if __name__ == '__main__':
-    test_files = ['./find_phone/60.jpg', './find_phone/61.jpg']
-    #   './data/find_phone/125.jpg', './data/find_phone/5.jpg', './data/find_phone/47.jpg',
-    #   './data/find_phone/48.jpg', './data/find_phone/109.jpg', './data/find_phone/41.jpg']
-    # 33, 125, 5, 47
-    #  60, 48, 109, 41
+    test_files = ['./find_phone/1.jpg']
+    # ['./find_phone/1.jpg', './find_phone/59.jpg', './find_phone/40.jpg', './find_phone/47.jpg', './find_phone/18.jpg', './find_phone/48.jpg', './find_phone/118.jpg', './find_phone/58.jpg', './find_phone/75.jpg', './find_phone/34.jpg', './find_phone/43.jpg', './find_phone/60.jpg', './find_phone/41.jpg', './find_phone/103.jpg', './find_phone/97.jpg'] 
+    
+    # 
     check_python_version()
     phone_detector = Phone_Detector(
         save_image=True, debug=True)
-    # test_files = glob.glob("./data/find_phone/*.jpg")
+    phone_detector.th = 59
+    # test_files = glob.glob("./find_phone/*.jpg")
     for test_file in test_files:
         test_image = cv2.imread(test_file)
         phone_detector.feed(test_image, test_file)
 
     phone_detector.summerize_result()
+
+
+#  1, 47, 58, 75, 43, 103,
