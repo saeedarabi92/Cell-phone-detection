e42e4d79b32149f9690ad9d243b0b76bf9b13504
saeedarabi92 <arabi@iastate.edu>
2021-02-07 01:21:49 -0600

    update find_phone.py

--- find_phone.py
+++ find_phone.py
@@ -13,10 +13,14 @@ if __name__ == '__main__':
     img_path = sys.argv[1]
     phone_detector = Phone_Detector(
         save_image=False, debug=False)
+
+    # setting the threshold for color filtering (This value found using a grid-based search approach. Forr more information checkout grid_search.py)
     phone_detector.th = 59
+
+    # loading the image to numpy array
     img = cv2.imread(img_path)
     try:
         x, y = phone_detector.feed(img, img_path)
         print(round(x, 4), " ", round(y, 4))
     except:
-        print("No detection resutl found for {}".format(img_path)) 
+        print("No detection resutl found for {}".format(img_path))
