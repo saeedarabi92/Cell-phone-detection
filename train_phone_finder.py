e418d8e7431f9cd471b2e2a9d1c9e327ea2f54f9
saeedarabi92 <arabi@iastate.edu>
2021-02-07 01:22:48 -0600

    Add train_phone_finder.py

---
+++ train_phone_finder.py
@@ -0,0 +1,18 @@
+'''
+ # @ Author: Saeed Arabi
+ # @ Create Time: 2021-02-03 15:46:05
+ # @ Email: arabi@iastate.edu
+ '''
+
+from helper import check_python_version
+
+if __name__ == "__main__":
+
+    check_python_version()
+    print("""
+    
+    This program was supposed to be for training the model. \n However, I was able to achieve acceptable result based on the requirement of the coding challenge, \n by only using pre-trained models and classic computer vision methods.
+
+    Please checkout the README.md file of the project for more informations!
+
+    """)
