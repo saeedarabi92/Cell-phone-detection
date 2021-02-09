ada04ea8b1c1614678666aeea4252e7307b80034
saeedarabi92 <arabi@iastate.edu>
2021-02-08 18:50:48 -0600

    update generate_detection_result.py

--- generate_detection_results.py
+++ generate_detection_results.py
@@ -14,7 +14,18 @@ if __name__ == '__main__':
     check_python_version()
     phone_detector = Phone_Detector(
         save_image=True, debug=True)
+
+    # downloding the deep model (if it's not already downloaded)
+    MODEL_NAME = 'faster_rcnn_nas_coco_2018_01_28'
+    phone_detector.PATH_TO_SAVED_MODEL = phone_detector.download_deep_learrning_model() + \
+        "/saved_model"
+
+    # loding the deep model
+    phone_detector.load_deep_model()
+
+    # setting the threshold for color filtering
     phone_detector.th = 59
+
     test_files = glob.glob("./find_phone/*.jpg")
     for test_file in test_files:
         test_image = cv2.imread(test_file)
